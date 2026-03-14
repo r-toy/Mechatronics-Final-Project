7b68031f094060d21665c6e74a545db1df92d9c2
jlane10 <jeffreylane10@gmail.com>
2026-03-14 16:13:15 -0700

    adafruit library stuff

--- src/adafruit.cpp
+++ src/adafruit.cpp
@@ -36,14 +36,4 @@ void loop(void) {
   // colorTemp = tcs.calculateColorTemperature(r, g, b);
   colorTemp = tcs.calculateColorTemperature_dn40(r, g, b, c);
   lux = tcs.calculateLux(r, g, b);
-
-  Serial.print("Color Temp: "); Serial.print(colorTemp, DEC); Serial.print(" K - ");
-  Serial.print("Lux: "); Serial.print(lux, DEC); Serial.print(" - ");
-  Serial.print("R: "); Serial.print(r, DEC); Serial.print(" ");
-  Serial.print("G: "); Serial.print(g, DEC); Serial.print(" ");
-  Serial.print("B: "); Serial.print(b, DEC); Serial.print(" ");
-  Serial.print("C: "); Serial.print(c, DEC); Serial.print(" ");
-  Serial.println(" ");
-  Serial.print("loop time: ");
-  Serial.println(millis() - prev_millis);
 }
